1a1f6569e9d568518f51da28a8e2218cbf757a8d
Mikhail Tonkushin <tonkushin@mail.ru>
2022-05-10 16:17:13 +0300

    added Taiga UI library

--- apps/aaa-core/src/app/app.module.ts
+++ apps/aaa-core/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TuiDialogModule,
+  TuiNotificationsModule,
+  TuiRootModule
+} from '@taiga-ui/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiNotificationsModule,
+    TuiDialogModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
